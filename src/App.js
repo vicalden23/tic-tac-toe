da37dd418c710bf95fa7479ba149970e6195c468
vic.alden23 <vic.alden23@gmail.com>
2019-03-10 13:51:27 -0700

    create board and game

--- src/App.js
+++ src/App.js
@@ -2,27 +2,79 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+function Square(props) {
+  return (
+    <button className='square' onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
+}
+
+
+class Board extends Component {
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    )
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div>
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          {this.renderSquare(3)}
+        </div>
+        <div>
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+        </div>
+        <div>
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
+        </div>
+      </div>
+    )
+  }
+}
+
+
+class Game extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      squares: Array(9).fill(''),
+      xIsNext: true
+    }
+  }
+
+  handleClick(i) {
+    let squares = this.state.squares.slice()
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    })
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <Board
+          squares={this.state.squares}
+          onClick={i => this.handleClick(i)}
+        />
       </div>
     );
   }
 }
 
-export default App;
+export default Game;
