52920d05018d86df77dbafe3345604e50d157766
vic.alden23 <vic.alden23@gmail.com>
2019-03-10 14:35:21 -0700

    determine winner and add reset button

--- src/App.js
+++ src/App.js
@@ -26,19 +26,19 @@ class Board extends Component {
     return (
       <div>
         <div>
+          {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
-          {this.renderSquare(3)}
         </div>
         <div>
+          {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
-          {this.renderSquare(6)}
         </div>
         <div>
+          {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
-          {this.renderSquare(9)}
         </div>
       </div>
     )
@@ -59,10 +59,37 @@ class Game extends Component {
   handleClick(i) {
     let squares = this.state.squares.slice()
     squares[i] = this.state.xIsNext ? 'X' : 'O'
+    this.calculateWinner(squares)
     this.setState({
       squares: squares,
       xIsNext: !this.state.xIsNext
     })
+  }
+
+  resetBoard() {
+    this.setState({
+      squares: Array(9).fill(null)
+    })
+  }
+
+  calculateWinner(board) {
+    let possibilities = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+
+    for (let i = 0; i < possibilities.length; i++) {
+      let [a, b, c] = possibilities[i]
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        alert(`${board[a]} wins!`)
+      }
+    }
   }
 
   render() {
@@ -72,6 +99,9 @@ class Game extends Component {
           squares={this.state.squares}
           onClick={i => this.handleClick(i)}
         />
+        <button className='reset-button' onClick={this.resetBoard.bind(this)}>
+          Reset
+        </button>
       </div>
     );
   }
